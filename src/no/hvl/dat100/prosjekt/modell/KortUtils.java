a4bf5d8b7b7af80c9999b144ab5e8458a4c15d86
600847 <august.helle1@gmail.com>
2021-10-11 17:28:52 +0200

    Ferdig 1.2 til 1.3

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -1,40 +1,63 @@
 package no.hvl.dat100.prosjekt.modell;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import java.util.Random;
 
 import no.hvl.dat100.prosjekt.TODO;
 
 public class KortUtils {
 
-	/**
-	 * Sorterer en samling. Rekkefølgen er bestemt av compareTo() i Kort-klassen.
-	 * 
-	 * @see Kort
-	 * 
-	 * @param samling
-	 * 			samling av kort som skal sorteres. 
-	 */
-	
-	public static void sorter(KortSamling samling) {
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		// TODO - END
-	}
-	
-	/**
-	 * Stokkar en kortsamling. 
-	 * 
-	 * @param samling
-	 * 			samling av kort som skal stokkes. 
-	 */
-	public static void stokk(KortSamling samling) {
-		
-		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.method());
-		// TODO - END
-	}
-	
+    /**
+     * Sorterer en samling. RekkefÃ¸lgen er bestemt av compareTo() i Kort-klassen.
+     * 
+     * @see Kort
+     * 
+     * @param samling
+     *             samling av kort som skal sorteres. 
+     */
+    
+    public static void sorter(KortSamling samling) {
+        
+        // TODO START
+        Kort[] alleKort = samling.getAllekort();
+
+        Arrays.sort(alleKort);
+        samling.fjernAlle();
+
+        for(Kort K: alleKort) {
+            samling.leggTil(K);
+        }
+    }
+           
+        // TODO - END
+           
+    
+         
+    /**
+     * Stokkar en kortsamling. 
+     * 
+     * @param samling
+     *             samling av kort som skal stokkes. 
+     */
+    public static void stokk(KortSamling samling) {
+        
+        // TODO - START
+        
+        Kort[] alleKort = samling.getAllekort();
+        Random rand = new Random();
+        for(int i = 0; i<alleKort.length; i++) {
+            int randomIndexToSwap = rand.nextInt(alleKort.length);
+            Kort temp = alleKort[randomIndexToSwap];
+            alleKort[randomIndexToSwap]= alleKort[i];
+            alleKort[i] =temp;
+            
+        }
+        
+        
+        // TODO - END
+    }
+    
 }
+
